6dd2830955a314eff4356972b3cdc2bab5cb7f8b
dataisland <dataisland@outlook.com>
2019-11-23 15:25:04 +0800

    Single issue mode

---
+++ src/cpu/dp/allocator.v
@@ -0,0 +1,103 @@
+`define INST_PORT_DEF(op, en_rx, en_ry, en_w, read_lockx, read_locky, read_datax, read_datay, read_addrx, read_addry, write_lock, write_addr)  \
+    input wire `oper_t op,              \
+    input wire en_rx,                   \
+    input wire en_ry,                   \
+    input wire en_w,                    \
+    input wire `regtag_t  read_lockx,   \
+    input wire `regtag_t  read_locky,   \
+    input wire `regtag_t  write_lock,   \
+    input wire `word_t read_datax,      \
+    input wire `word_t read_datay,      \
+    input wire `regaddr_t read_addrx,   \
+    input wire `regaddr_t read_addry,   \
+    input wire `regaddr_t write_addr,
+
+`define MODIFY_VAR_DEF(en_w, addr, tag)     \
+    output reg en_w,                        \
+    output reg `regaddr_t addr,             \
+    output reg `regtag_t tag,               
+
+module allocator(
+    `INST_PORT_DEF(
+        op0, en_rx0, en_ry0, en_w0, 
+        read_lockx0, read_locky0, 
+        read_datax0, read_datay0, 
+        read_addrx0, read_addry0, 
+        write_lock0, write_addr0)
+    `INST_PORT_DEF(
+        op1, en_rx1, en_ry1, en_w1, 
+        read_lockx1, read_locky1, 
+        read_datax1, read_datay1, 
+        read_addrx1, read_addry1, 
+        write_lock1, write_addr1)
+
+    // Update Reg State
+    `MODIFY_VAR_DEF(en_mw0, maddr0, mtag0)
+    `MODIFY_VAR_DEF(en_mw1, maddr1, mtag1)
+    
+    input wire alu0_busy,
+    input wire `regtag_t alu0_tagx_in,
+    input wire `regtag_t alu0_tagy_in,
+    input wire `regtag_t alu0_tagw_in,
+
+    input wire alu1_busy,
+    input wire `regtag_t alu1_tagx_in,
+    input wire `regtag_t alu1_tagy_in,
+    input wire `regtag_t alu1_tagw_in,
+
+    output reg alu0_en_out,
+    output reg `sinst_t alu0_op_out,
+    output reg `regtag_t alu0_tagx_out,
+    output reg `regtag_t alu0_tagy_out,
+    output reg `regtag_t alu0_tagw_out,
+    output reg `word_t alu0_datax_out,
+    output reg `word_t alu0_datay_out,
+    output reg `addr_t alu0_addrw_out,
+
+    output reg alu1_en_out,
+    output reg `sinst_t alu1_op_out,
+    output reg `regtag_t alu1_tagx_out,
+    output reg `regtag_t alu1_tagy_out,
+    output reg `regtag_t alu1_tagw_out,
+    output reg `word_t alu1_datax_out,
+    output reg `word_t alu1_datay_out,
+    output reg `addr_t alu1_addrw_out,
+
+    input wire branch_busy,
+    input wire ls_busy,
+
+    input wire clk,
+    input wire rst,
+    input wire rdy
+);
+
+reg [0 : `ALU_CNT + `BRANCH_CNT + `LS_CNT - 1] busy;
+
+integer i;
+
+always @(negedge clk) begin
+    if (op0 != `OP_NOP) begin
+        case (op0[7 : 4])
+        4'b0001, 4'b0010, 4'b0101: begin
+            if (alu0_busy == 0 || 
+                (alu0_busy == 1 && alu0_tagx_in == `UNLOCKED 
+                                && alu0_tagy_in == `UNLOCKED
+                                && alu0_tagw_in == `UNLOCKED)) begin
+                alu0_en_out     <= 1;
+                alu0_op_out     <= op0[3 : 0];
+                alu0_tagx_out   <= en_rx0 ? read_lockx0: `UNLOCKED;
+                alu0_datax_out  <= en_rx0 ? read_datax0: `ZERO_WORD;
+                alu0_tagy_out   <= en_ry0 ? read_locky0: `UNLOCKED;
+                alu0_datay_out  <= en_ry0 ? read_datay0: `ZERO_WORD;
+                alu0_tagw_out   <= en_w0  ? write_lock0: `UNLOCKED;
+                alu0_addrw_out  <= write_addr0;
+                en_mw0 <= 1;
+                maddr0 <= write_addr0;
+                mtag0  <= `ALU_MASTER;
+            end
+        end
+        endcase
+    end
+end
+
+endmodule // allocator
--- src/cpu/id/reg_stat.v
+++ src/cpu/id/reg_stat.v
@@ -1,5 +1,5 @@
-`include "../../common_defs.vh"
-`include "../cpu_defs.vh"
+`include "common_defs.vh"
+`include "cpu/cpu_defs.vh"
 
 `define READ_VAR_DEFINE(_en_r, _reg_read_addr, _data, _lock)\
     input wire _en_r,                                       \
@@ -12,30 +12,59 @@
     input wire `regaddr_t _reg_write_addr,                      \
     input wire `word_t _write_data,
 
+`define WRITE_TAG_DEFINE(_en_w, _reg_write_addr, _write_data)   \
+    input wire _en_w,                                           \
+    input wire `regaddr_t _reg_write_addr,                      \
+    input wire `regtag_t _write_data,
+
+`define FROM_WRITE(addr) (en_mod0 && reg_addr0 == addr) ? reg_tag0 : (en_mod1 && reg_addr1 == addr) ? reg_tag1
+
 `define REG_OUTPUT(_en_r, _reg_read_addr, _data, _lock) \
 if (_en_r) begin                                        \
-    _lock <= lock[_reg_read_addr];                      \
-    _data <= lock[_reg_read_addr] == UNLOCKED ? data[_reg_read_addr]: _reg_read_addr;   \ 
+    _lock <= `FROM_WRITE(_reg_read_addr) : lock[_reg_read_addr];                                                        \
+    _data <= (`FROM_WRITE(_reg_read_addr) : lock[_reg_read_addr]) == `UNLOCKED ? data[_reg_read_addr]: _reg_read_addr;  \ 
 end
 
 `define REG_WRITE(_en_w, _reg_write_addr, _data)    \
 if (_en_w) begin                                    \
+    lock[_reg_write_addr] <= `UNLOCKED;             \
     data[_reg_write_addr] <= _data;                 \
 end
 
+`define REG_TAG_WRITE(_en_w, _addr, _tag)   \
+if (_en_w) begin                            \
+    lock[_addr] <= _tag;                    \
+end
+
 module reg_stat(
-    `READ_VAR_DEFINE(en_r0, reg_read_addr0, read_data0, lock0)
-    `READ_VAR_DEFINE(en_r1, reg_read_addr1, read_data1, lock1)
-    `READ_VAR_DEFINE(en_r2, reg_read_addr2, read_data2, lock2)
-    `READ_VAR_DEFINE(en_r3, reg_read_addr3, read_data3, lock3)
+    input wire `word_t imm0, imm1,
+    `READ_VAR_DEFINE(en_rx0, reg_read_addrx0, read_datax0, lockx0)
+    `READ_VAR_DEFINE(en_ry0, reg_read_addry0, read_datay0, locky0)
+    `READ_VAR_DEFINE(en_rx1, reg_read_addrx1, read_datax1, lockx1)
+    `READ_VAR_DEFINE(en_ry1, reg_read_addry1, read_datay1, locky1)
     
+    input en_rw0,
+    input wire `regaddr_t addrw0,
+    output reg `regtag_t lockw0,
+
+    input en_rw1,
+    input wire `regaddr_t addrw1,
+    output reg `regtag_t lockw1,
+
+    // From Execute and L/S
+    // Update Data (no conflict)
     `WRITE_VAR_DEFINE(en_w0, reg_write_addr0, write_data0)
     `WRITE_VAR_DEFINE(en_w1, reg_write_addr1, write_data1)
-    // `WRITE_VAR_DEFINE(en_w2, reg_write_addr2, write_data2)
-    // `WRITE_VAR_DEFINE(en_w3, reg_write_addr3, write_data3)
+    `WRITE_VAR_DEFINE(en_w2, reg_write_addr2, write_data2)
+
+    // From allocator
+    // Update Tag (no conflict)
+    `WRITE_TAG_DEFINE(en_mod0, reg_addr0, reg_tag0)
+    `WRITE_TAG_DEFINE(en_mod1, reg_addr1, reg_tag1)
 
     input wire clk,
-    input wire rst
+    input wire rst,
+    input wire rdy
 );
 
 reg `word_t data[0 : `REG_COUNT-1];
@@ -48,16 +77,29 @@ always @(posedge clk) begin
             data[i] <= `ZERO_WORD;
             lock[i] <= `UNLOCKED;
         end
-    end else begin
-        `REG_OUTPUT(en_r0, reg_read_addr0, read_data0, lock0)
-        `REG_OUTPUT(en_r1, reg_read_addr1, read_data1, lock1)
-        `REG_OUTPUT(en_r2, reg_read_addr2, read_data2, lock2)
-        `REG_OUTPUT(en_r3, reg_read_addr3, read_data3, lock3)
+    end else if(rdy) begin
+        `REG_OUTPUT(en_rx0, reg_read_addrx0, read_datax0, lockx0) else read_datax0 <= imm0;
+        `REG_OUTPUT(en_ry0, reg_read_addry0, read_datay0, locky0)
+        `REG_OUTPUT(en_rx1, reg_read_addrx1, read_datax1, lockx1) else read_datax1 <= imm1;
+        `REG_OUTPUT(en_ry1, reg_read_addry1, read_datay1, locky1)
 
+        if (en_rw0) begin
+            lockw0 <= `FROM_WRITE(addrw0) : lock[addrw0];
+        end
+        if (en_rw1) begin
+            lockw1 <= `FROM_WRITE(addrw1) : lock[addrw1];
+        end
+        
+        `REG_TAG_WRITE(en_mod0, reg_addr0, reg_tag0)
+        `REG_TAG_WRITE(en_mod1, reg_addr1, reg_tag1)
+    end
+end
+
+always @(negedge clk) begin
+    if (rdy) begin
         `REG_WRITE(en_w0, reg_write_addr0, write_data0)
         `REG_WRITE(en_w1, reg_write_addr1, write_data1)
         `REG_WRITE(en_w2, reg_write_addr2, write_data2)
-        `REG_WRITE(en_w3, reg_write_addr3, write_data3)
     end
 end
  
