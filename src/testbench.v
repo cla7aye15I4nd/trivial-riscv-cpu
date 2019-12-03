dd5b1ff945d6d7157d1a8749d155271898b2cb6f
dataisland <dataisland@outlook.com>
2019-12-04 00:57:11 +0800

    Create testbench.v

---
+++ src/testbench.v
@@ -0,0 +1,30 @@
+// testbench top module file
+// for simulation only
+`include "riscv_top.v"
+`timescale 1ns/1ps
+module testbench;
+
+reg clk;
+reg rst;
+
+riscv_top #(.SIM(1)) top(
+    .EXCLK(clk),
+    .btnC(rst),
+    .Tx(),
+    .Rx(),
+    .led()
+);
+
+initial begin
+  // $dumpfile("test.vcd");
+  // $dumpvars(0, top);
+  clk=0;
+  rst=1;
+  repeat(50) #1 clk=!clk;
+  rst=0; 
+  forever #1 clk=!clk;
+
+  $finish;
+end
+
+endmodule
