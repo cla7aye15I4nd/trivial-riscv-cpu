4afb9763d5dd599f05f3f0d692e250c12c70395e
dataisland <dataisland@outlook.com>
2019-12-06 23:17:48 +0800

    Add argument option

--- test/judge.py
+++ test/judge.py
@@ -1,18 +1,23 @@
 import os
+import sys
 import shutil
+import string
 
-for f in os.listdir('package'):
+package = 'package' if len(sys.argv) == 1 else sys.argv[1]
+tmp = 'tmp' if len(sys.argv) <= 2 else sys.argv[2]
+
+for f in os.listdir(package):
     name = f[:-2]
     if f[-2:] == ".c":
         yn = input(f"Do you need test {name}.c?(y/n)")
         if yn.lower() != 'n':
             print(f"Start test {name}..")
-            shutil.copy('tb.o', os.path.join('tmp','tb.o'))
-            shutil.copy(os.path.join('package', f'{name}.data'),
-                        os.path.join('tmp', 'test.data'))
-            shutil.copy(os.path.join('package', f'{name}.ans'),
-                        os.path.join('tmp', 'test.ans'))
-            os.chdir('tmp')
+            shutil.copy('tb.o', os.path.join(tmp,'tb.o'))
+            shutil.copy(os.path.join(package, f'{name}.data'),
+                        os.path.join(tmp, 'test.data'))
+            shutil.copy(os.path.join(package, f'{name}.ans'),
+                        os.path.join(tmp, 'test.ans'))
+            os.chdir(tmp)
             os.system('vvp tb.o > tb.out')
             with open('tb.out') as f:
                 text = f.read()
