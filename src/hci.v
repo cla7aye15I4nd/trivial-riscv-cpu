f333e6ae5be9e2dda3a3d474a7f3c89c11782289
dataisland <dataisland@outlook.com>
2019-12-07 11:53:39 +0800

    Get Runtime

--- src/hci.v
+++ src/hci.v
@@ -247,7 +247,7 @@ always @*
               d_tx_data = 8'h00;
               d_wr_en = 1'b1;
             end
-            $display("RUNTIME : ", $realtime, "s");
+            $write("runtime : %fs", $realtime);
             $finish;
           end
         endcase
