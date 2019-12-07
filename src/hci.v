907fd2188df1baf3c6f53a930425379db3a6df05
dataisland <dataisland@outlook.com>
2019-12-07 23:06:27 +0800

    Fix runtime display

--- src/hci.v
+++ src/hci.v
@@ -247,7 +247,7 @@ always @*
               d_tx_data = 8'h00;
               d_wr_en = 1'b1;
             end
-            $write("runtime : %ts", $realtime);
+            $write("runtime : %tns", $realtime);
             $finish;
           end
         endcase
