21e7e5b602a6fb6b220e459364c75e2f8a8798e5
dataisland <dataisland@outlook.com>
2019-12-14 01:16:52 +0800

    Opt Fetch (4 periods)

--- src/cache/cache.v
+++ src/cache/cache.v
@@ -1,12 +1,10 @@
-module cache 
-#(
+module cache #(
     parameter LINE_WIDTH  = 32,
     parameter BLOCK_WIDTH = 6,
     parameter ADDR_WIDTH = 13,
     parameter BLOCK_SIZE  = 2**BLOCK_WIDTH,
     parameter TAG_WIDTH   = ADDR_WIDTH - BLOCK_WIDTH - 2
-)
-(
+)(
     input wire clk,
     input wire rst,
     input wire rdy,
@@ -44,213 +42,150 @@ reg [TAG_WIDTH - 1: 0]  tag1[0 : BLOCK_SIZE-1];
 reg [LINE_WIDTH - 1: 0] idata0[0 : BLOCK_SIZE-1];
 reg [LINE_WIDTH - 1: 0] idata1[0 : BLOCK_SIZE-1];
 
-reg `addr_t instx_addr_p, insty_addr_p;
-reg `addr_t instx_addr_n, insty_addr_n;
-
-wire [TAG_WIDTH - 1 : 0] pcx_tag, pcy_tag;
-wire [BLOCK_WIDTH - 1 : 0] pcx_index, pcy_index;
-reg [BLOCK_WIDTH - 1 : 0] addr_index;
-
-// Instruction fetch
-assign pcx_tag = pcx[ADDR_WIDTH-1 : ADDR_WIDTH - TAG_WIDTH];
-assign pcy_tag = pcy[ADDR_WIDTH-1 : ADDR_WIDTH - TAG_WIDTH];
-
-assign pcx_index = pcx[ADDR_WIDTH-1 - TAG_WIDTH : 2];
-assign pcy_index = pcy[ADDR_WIDTH-1 - TAG_WIDTH : 2];
+reg `addr_t instx_addr, insty_addr;
+reg  [BLOCK_WIDTH - 1 : 0] addr_index;
 
 // load / store
-reg `byte_t mode;
 reg         data_oper;
-reg `addr_t data_addr_p, data_addr_n;
+reg `addr_t data_addr;
 reg `byte_t data_size;
 reg `word_t data_data;
 
-reg `word_t counter;
-reg `addr_t addr;
-reg `word_t size;
-reg `word_t data;
+reg         save0, save1;
+reg `word_t cont0, cont1;
+reg         oper0, oper1;
+reg `addr_t addr0, addr1;
+reg `word_t size0, size1;
+reg `word_t data , data1;
 
-wire `byte_t mem_data;
+wire [TAG_WIDTH - 1 : 0]   pcx_tag, pcy_tag, stag;
+wire [BLOCK_WIDTH - 1 : 0] pcx_index, pcy_index, sindex;
+wire hitx0, hitx1, hity0, hity1;
+
+assign sindex = addr0[ADDR_WIDTH-1 - TAG_WIDTH : 2];
+assign stag = addr0[ADDR_WIDTH-1 : ADDR_WIDTH - TAG_WIDTH];
+
+assign pcx_index = pcx[ADDR_WIDTH-1 - TAG_WIDTH : 2];
+assign pcy_index = pcy[ADDR_WIDTH-1 - TAG_WIDTH : 2];
+assign pcx_tag = pcx[ADDR_WIDTH-1 : ADDR_WIDTH - TAG_WIDTH];
+assign pcy_tag = pcy[ADDR_WIDTH-1 : ADDR_WIDTH - TAG_WIDTH];
+
+assign hitx0 = valid0[pcx_index] && tag0[pcx_index] == pcx_tag;
+assign hitx1 = valid1[pcx_index] && tag1[pcx_index] == pcx_tag;
+assign hity0 = valid0[pcy_index] && tag0[pcy_index] == pcy_tag;
+assign hity1 = valid1[pcy_index] && tag1[pcy_index] == pcy_tag;
+
+wire `word_t mem_data;
 assign mem_data = data_in;
- 
-integer i;
+
 always @(posedge clk) begin
     if (rst || ~rdy) begin
-        instx_addr_p  <= `NULL_PTR;
-        insty_addr_p  <= `NULL_PTR;
-        data_addr_p   <= `NULL_PTR;
-        in_fifo     <= 0;
+        hitx <= 0;
+        hity <= 0;
+        instx <= `OP_NOP;
+        insty <= `OP_NOP;
+        valid0 <= 0;
+        valid1 <= 0;
+        data_addr <= `NULL_PTR;
+        instx_addr <= `NULL_PTR;
+        insty_addr <= `NULL_PTR;
+        addr0 <= `NULL_PTR;
+        addr1 <= `NULL_PTR;
     end else begin
-        if (en_ls && data_addr_n == `NULL_PTR) begin
+        hitx <= hitx0 | hitx1;
+        hity <= hity0 | hity1;
+        instx <= hitx0? idata0[pcx_index]: idata1[pcx_index];
+        insty <= hity0? idata0[pcy_index]: idata1[pcy_index];
+
+        if (~hitx0 && ~hitx1 && addr0 != pcx && addr1 != pcx && instx_addr == `NULL_PTR) instx_addr <= pcx;
+        if (~hity0 && ~hity1 && addr0 != pcy && addr1 != pcy && insty_addr == `NULL_PTR) insty_addr <= pcy;
+
+        if (en_ls && data_addr == `NULL_PTR) begin
+            in_fifo   <= 1;
             data_oper <= ls_oper;
-            data_addr_p <= ls_addr;
+            data_addr <= ls_addr;
             data_size <= ls_size;
             data_data <= ls_data;
-            in_fifo   <= 1;
         end else begin
-            data_addr_p <= data_addr_n;
             in_fifo   <= 0;
         end
 
-        if (en_rx) begin
-            if (valid0[pcx_index] && tag0[pcx_index] == pcx_tag) begin
-                hitx <= 1;
-                instx <= idata0[pcx_index];
-                instx_addr_p <= `NULL_PTR;
-            end else if (valid1[pcx_index] && tag1[pcx_index] == pcx_tag) begin
-                hitx <= 1;
-                instx <= idata1[pcx_index];
-                instx_addr_p <= `NULL_PTR;
-            end else begin
-                hitx <= 0;
-                instx <= `ZERO;
-                if (instx_addr_n == `NULL_PTR && pcx != addr)
-                    instx_addr_p <= pcx;
-            end
-        end else begin
-            hitx <= 0;
-        end
+        save0 <= save1;
+        cont0 <= cont1;
+        addr0 <= addr1;
+        oper0 <= oper1;
+        size0 <= size1;
 
-        if (en_ry) begin
-            if (valid0[pcy_index] && tag0[pcy_index] == pcy_tag) begin
-                hity <= 1;
-                insty <= idata0[pcy_index];
-                insty_addr_p <= `NULL_PTR;
-            end else if (valid1[pcy_index] && tag1[pcy_index] == pcy_tag) begin
-                hity <= 1;
-                insty <= idata1[pcy_index];
-                insty_addr_p <= `NULL_PTR;
-            end else begin
-                hity <= 0;
-                insty <= `ZERO;
-                if (insty_addr_n == `NULL_PTR && pcy != addr) 
-                    insty_addr_p <= pcy;
-            end
+        if (addr1 != `NULL_PTR && cont1 != size1) begin
+            cont1 <= cont1 + 1;
+            data1 <= data1[31 : 8];
+            addr_out <= addr1 + cont1;
+            r_nw_out <= oper1;
+            data_out <= data1[7 : 0];
         end else begin
-            hity <= 0;
-        end
-    end
-end
-
-always @(negedge clk) begin
-    if (rst || ~rdy) begin
-        valid0 <= 0;
-        valid1 <= 0;
-        mode <= 2'b11;
-        addr <= `NULL_PTR;
-        data <= `ZERO;
-        instx_addr_n <= `NULL_PTR;
-        insty_addr_n <= `NULL_PTR;
-        data_addr_n <= `NULL_PTR;
-        counter     <= 0;
-    end else /*if (rdy)*/ begin  
-        if (mode != 2'b11) begin
-            if (size - counter > 1) begin
-                if (mode == 2'b01) begin
-                    data_out  <= data[7 : 0];
-                    data      <= data[31 : 8];
-                    r_nw_out  <= `WRITE_SIGNAL;
+            cont1 <= 1;
+            // fetch new addr
+            if (addr1[17] == 1'b1 && ~oper1) begin
+                addr1 <= `NULL_PTR;
+            end else if (data_addr != `NULL_PTR) begin
+                if (addr1 != `NULL_PTR) begin
+                    addr1 <= `NULL_PTR;
+                    addr_out <= 0;
+                    r_nw_out <= `READ_SIGNAL;
                 end else begin
-                    data <= {data, mem_data};
-                    r_nw_out  <= `READ_SIGNAL;
+                    save1 <= 0;
+                    addr1 <= data_addr;
+                    oper1 <= data_oper;
+                    size1 <= data_size;
+                    data_addr <= `NULL_PTR;
+                    data1 <= data_data[31 : 8];
+                    r_nw_out <= data_oper;
+                    addr_out <= data_addr;
+                    data_out <= data_data[7 : 0];
                 end
-                finish    <= 0;
-                counter   <= counter + 1;
-                addr_out  <= addr + counter + 1;                
-                data_addr_n <= data_addr_p;
+            end else if (instx_addr != `NULL_PTR) begin
+                save1 <= 1;
+                addr1 <= instx_addr;
+                instx_addr <= `NULL_PTR;
+                oper1 <= `READ_SIGNAL;
+                size1 <= 4;
+                r_nw_out <= `READ_SIGNAL;
+                addr_out <= instx_addr;
+            end else if (insty_addr != `NULL_PTR) begin
+                save1 <= 1;
+                addr1 <= insty_addr;
+                insty_addr <= `NULL_PTR;
+                oper1 <= `READ_SIGNAL;
+                size1 <= 4;
+                r_nw_out <= `READ_SIGNAL;
+                addr_out <= insty_addr;
             end else begin
-                counter   <= 0;
-                if (mode == 2'b10) begin
-                    finish <= 0;
-                    data_addr_n <= data_addr_p;
-                    // Put read_data in Cache
-                    if (valid0[addr_index] == 0) begin
-                        valid0[addr_index] <= 1;
-                        tag0[addr_index] <= addr[ADDR_WIDTH-1 : ADDR_WIDTH - TAG_WIDTH];
-                        idata0[addr_index] <= {data, mem_data};
+                addr1 <= `NULL_PTR;
+                addr_out <= 0;
+                r_nw_out <= `READ_SIGNAL;
+            end
+        end 
+        
+        if (addr0 != `NULL_PTR && ~oper0) begin
+            data <= {data, data_in};
+            if (cont0 == size0) begin
+                if (save0) begin
+                    if (~valid0[sindex]) begin
+                        valid0[sindex] <= 1;
+                        tag0[sindex] <= stag;
+                        idata0[sindex] <= {data, data_in};
                     end else begin
-                        valid1[addr_index] <= 1;
-                        tag1[addr_index] <= addr[ADDR_WIDTH-1 : ADDR_WIDTH - TAG_WIDTH];
-                        idata1[addr_index] <= {data, mem_data};
-                    end 
-                end else if (mode == 2'b00) begin
-                    ls_data_out <= {data, mem_data};
-                    finish <= 1;                
-                    data_addr_n <= data_addr_p;
+                        valid1[sindex] <= 1;
+                        tag1[sindex] <= stag;
+                        idata1[sindex] <= {data, data_in};
+                    end
                 end else begin
-                    finish <= 0;
-                    data_addr_n <= data_addr_p;
+                    ls_data_out <= {data, data_in};      
                 end
-
-                // Fetch new addr
-                if (data_addr_p != `NULL_PTR) begin                                   
-                    mode <= data_oper == `READ_SIGNAL ? 2'b00: 2'b01;                  
-                    addr <= data_addr_p;
-                    size <= data_size;                                              
-                    data <= data_data[31 : 8];   
-                    r_nw_out  <= data_oper;
-                    data_out  <= data_data[7 : 0];   
-                    addr_out  <= data_addr_p;                             
-                    data_addr_n <= `NULL_PTR;          
-                end else if (instx_addr_p != `NULL_PTR) begin             
-                    mode <= 2'b10;                                      
-                    addr <= instx_addr_p;                                 
-                    size <= 4;                                          
-                    data <= `ZERO;                                      
-                    r_nw_out   <= `READ_SIGNAL;                         
-                    addr_out   <= instx_addr_p;                           
-                    instx_addr_n <= `NULL_PTR; 
-                    addr_index <= instx_addr_p[ADDR_WIDTH-1 - TAG_WIDTH : 2];       
-                end else if (insty_addr_p != `NULL_PTR) begin             
-                    mode <= 2'b10;                                      
-                    addr <= insty_addr_p;                                 
-                    size <= 4;                                          
-                    data <= `ZERO;                                      
-                    r_nw_out   <= `READ_SIGNAL;                         
-                    addr_out   <= insty_addr_p;                           
-                    insty_addr_n <= `NULL_PTR;
-                    addr_index <= insty_addr_p[ADDR_WIDTH-1 - TAG_WIDTH : 2];       
-                end else begin                                          
-                    mode <= 2'b11;                                      
-                    addr <= `NULL_PTR;                                  
-                    r_nw_out   <= `READ_SIGNAL;   
-                end
-
             end
+            finish <= (cont0 == size0 && ~save0) ? ~oper0: 0;
         end else begin
-            counter <= 0;    
-            if (data_addr_p != `NULL_PTR) begin                                   
-                mode <= data_oper == `READ_SIGNAL ? 2'b00: 2'b01;                  
-                addr <= data_addr_p;
-                size <= data_size;                                              
-                data <= data_data[31 : 8];   
-                r_nw_out  <= data_oper;
-                data_out  <= data_data[7 : 0];   
-                addr_out  <= data_addr_p;                             
-                data_addr_n <= `NULL_PTR;          
-            end else if (instx_addr_p != `NULL_PTR) begin             
-                mode <= 2'b10;                                      
-                addr <= instx_addr_p;                                 
-                size <= 4;                                          
-                data <= `ZERO;                                      
-                r_nw_out   <= `READ_SIGNAL;                         
-                addr_out   <= instx_addr_p;                           
-                instx_addr_n <= `NULL_PTR; 
-                addr_index <= instx_addr_p[ADDR_WIDTH-1 - TAG_WIDTH : 2];       
-            end else if (insty_addr_p != `NULL_PTR) begin             
-                mode <= 2'b10;                                      
-                addr <= insty_addr_p;                                 
-                size <= 4;                                          
-                data <= `ZERO;                                      
-                r_nw_out   <= `READ_SIGNAL;                         
-                addr_out   <= insty_addr_p;                           
-                insty_addr_n <= `NULL_PTR;
-                addr_index <= insty_addr_p[ADDR_WIDTH-1 - TAG_WIDTH : 2];       
-            end else begin                                          
-                mode <= 2'b11;                                      
-                addr <= `NULL_PTR;                                  
-                r_nw_out   <= `READ_SIGNAL;   
-            end
+            finish <= 0;
         end
     end
 end
